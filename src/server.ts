77e853e4be17c338413906c2a365ab611529d9ea
Pandiyaraj S. <UT01117@unicomtic.com>
2024-11-04 18:05:35 +0530

    2nd uplode

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import { IncomingMessage, ServerResponse } from 'http';
 import * as http from 'http';
 
 
@@ -5,13 +6,19 @@ const hostname: string = '127.0.0.1';
 const port: number = 3000;
 
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello , TypeScript Node.js HTTP Server!\n');
-});
+    res.end('Hello, TypeScript Node.js HTTP Server with types!\n');
+  });
+  
 
-
-server.listen(port, hostname, () => {
+server.listen(port, hostname, function()  {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+ 
